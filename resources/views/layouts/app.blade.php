d53f8e2a5ddf13bda8f550dbad582b1da445a940
DESKTOP-1FAHU9S\malki <borislavkostadinov00@gmail.com>
2025-04-13 23:30:15 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Катедра Маркетинг - БТУ</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/department_logo.png') }}">
+    <link rel="shortcut icon" href="{{ asset('images/department_logo.png') }}" type="image/png">
+    <link rel="apple-touch-icon" href="{{ asset('images/department_logo.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
